f4aa2444a4e898780265dfab25bac0e36f3090a9
Abraham Abin Jo (uib06040) <uib06040@frl8dmmu.conti.de>
2018-10-05 12:26:43 +0200

    Seventh to commit the GUI code along with the code published by the PolyrnnPP team to GitHub

---
+++ Cleanup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf *.png
+rm -rf imgs/*.png
+rm -rf output/*.png
+rm -rf output/*.json
+
--- src/demo_inference.sh
+++ src/demo_inference.sh
@@ -7,12 +7,12 @@ mv imgs/crop_img*.png dmps/
 
 
 python src/inference.py \
-    --PolyRNN_metagraph='models/poly/polygonplusplus.ckpt.meta' \
-    --PolyRNN_checkpoint='models/poly/polygonplusplus.ckpt' \
-    --EvalNet_checkpoint='models/evalnet/evalnet.ckpt' \
+    --PolyRNN_metagraph='../polyrnn/models/poly/polygonplusplus.ckpt.meta' \
+    --PolyRNN_checkpoint='../polyrnn/models/poly/polygonplusplus.ckpt' \
+    --EvalNet_checkpoint='../polyrnn/models/evalnet/evalnet.ckpt' \
     --InputFolder='imgs/' \
-    --GGNN_checkpoint='models/ggnn/ggnn.ckpt' \
-    --GGNN_metagraph='models/ggnn/ggnn.ckpt.meta' \
+    --GGNN_checkpoint='../polyrnn/models/ggnn/ggnn.ckpt' \
+    --GGNN_metagraph='../polyrnn/models/ggnn/ggnn.ckpt.meta' \
     --OutputFolder='output/' \
     --Use_ggnn=True
 
