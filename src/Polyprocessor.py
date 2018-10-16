39f8411b51d1f2e2e9f20a44f65c854a46737bfa
Abraham Abin Jo (uib06040) <uib06040@frl8dmmu.conti.de>
2018-10-16 15:51:20 +0200

    F

---
+++ src/Polyprocessor.py
@@ -0,0 +1,36 @@
+import glob
+import json
+import os
+import skimage.io as io
+import tensorflow as tf
+import cv2
+import tqdm
+import numpy as np
+
+flags = tf.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('OutputFolder', '../output/', 'OutputFolder')
+
+
+preds_path = glob.glob(os.path.join(FLAGS.OutputFolder, '*.json'))
+for pred_path in tqdm.tqdm(preds_path):
+    pred = json.load(open(pred_path, 'r'))
+    file_name = pred_path.split('/')[-1].split('.')[0]
+    im_crop = cv2.imread(pred['img_source'])
+    polys = np.array(pred['polys'])
+    pix = np.multiply(polys,100)
+    pix = np.around(pix,decimals=0)
+    print('Polys::',pix)
+    dim = (224,224)
+    resized = cv2.resize(im_crop, dim, interpolation = cv2.INTER_AREA)
+    print('size of image:',resized.shape)
+    cv2.imshow("test",im_crop)
+    #polyspix = np.array(pred['polys']*100,np.uint8)
+    cv2.polylines(im_crop,(np.int32(pix)),True,(0,255,255))
+    #cv2.fillPoly(im_crop, np.array(pix, dtype=np.int32), (0,255,255))
+    winname = 'example'
+    cv2.namedWindow(winname)
+    cv2.imshow(winname, img)
+    cv2.waitKey()
+    cv2.destroyWindow(winname)
